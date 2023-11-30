f79ecc4361a07d73f3f7d8caabcfe381e2c70160
ctossou <cyriaque.tossou@etudiant.univ-rennes1.fr>
2023-11-30 15:16:21 +0100

    add_template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppingBasketComponent } from './shopping-basket/shopping-basket.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children: [
+                    {path: "home", component: HomeComponent},
+                    {path: "basket", component: ShoppingBasketComponent},
+                  ],
+      }
+];
+
+
+
+// {
+//     path: '',
+//     component:AppComponent, 
+//     children: [
+//         {path: "home", component: HomeComponent},
+//        // {path: "tests", component: AppComponent},
+//     ],
+//    // loadChildren: () => import('./home/home.component').then(m => m.HomeComponent),
+//   }
---
+++ src/app/shopping-basket/shopping-basket.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-shopping-basket',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shopping-basket.component.html',
+  styles: ``
+})
+export class ShoppingBasketComponent {
+
+}
